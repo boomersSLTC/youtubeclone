674b5d376a2ffa705af9ea82b8e69e5a9c588ab9
boomersSLTC <offiwallet@gmail.com>
2023-06-01 13:30:50 +0530

    file structure

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Feed from './Feed';
+import ChannelDetail from './ChannelDetail';
+import Navbar from './Navbar';
+import SearchFeed from './SearchFeed';
+import VideoDetail from './VideoDetail';
+
+export {
+    Feed,
+    ChannelDetail,
+    Navbar,
+    SearchFeed,
+    VideoDetail
+}
